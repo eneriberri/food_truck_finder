344332d4bc62352f161aa0eaba3c0b872431f044
Irene Nguyen <irene.m.nguyen@gmail.com>
2014-03-07 21:43:02 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodTruckFinder::Application.config.secret_token = 'c89b084fe0805548bdd5fe75142d937b805d0dcc10c9cafaf9128bc578cf16f2a747b4b93cf53e752295f3a330a42476c13aaf0dea5a724e115d69e1c94bd091'

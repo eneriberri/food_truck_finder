e16d7bf687b3bf529205e333c0fa07049f074edb
Irene Nguyen <irene.m.nguyen@gmail.com>
2014-03-08 15:44:32 -0800

    input form

--- app/assets/javascripts/views/index.js
+++ app/assets/javascripts/views/index.js
@@ -1,10 +1,19 @@
 FoodTruckFinder.Views.Index = Backbone.View.extend({
   template: JST['index'],
+  
+  events: {
+    'click button': 'findFood'
+  },
 
   render: function() {
     var renderedHTML = this.template({trucks: this.collection});
     console.log(this.collection);
     this.$el.html(renderedHTML);
     return this;
+  },
+  
+  findFood: function(e) {
+    e.preventDefault();
+    console.log('yes');
   }
 })
